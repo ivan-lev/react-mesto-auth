e99104902ed273cab13d8a69a21ade61b73d8b52
Ivan Lev <fanatos@mail.ru>
2023-11-15 16:39:04 +0500

    feat: ркализовать в хедере отображение информации

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+function Header({ loggedIn }) {
+  return (
+    <header className="header">
+      <a href="#" className="header__logo"></a>
+      <Routes>
+        <Route
+          path="/sign-up"
+          element={
+            <Link className={styles['header-link']} to="/sign-in">
+              Войти
+            </Link>
+          }
+        ></Route>
+        <Route
+          path="/sign-in"
+          element={
+            <Link className={styles['header-link']} to="/sign-up">
+              Зарегистрироваться
+            </Link>
+          }
+        ></Route>
+      </Routes>
+    </header>
+  );
+}
+
+export default Header;
