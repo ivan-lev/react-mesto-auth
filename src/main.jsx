2a2ce9433c97312ee1edb1c27d0e37e09d4ec0a2
Ivan Lev <fanatos@mail.ru>
2023-11-15 11:21:29 +0500

    feat: обернуть приложение в BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './main.css';
 import App from './components/App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
