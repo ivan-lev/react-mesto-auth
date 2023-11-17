13122defecaf2b60786fc7f7b0670ffe6b9ceb21
Ivan Lev <navilev@yandex.ru>
2023-11-17 12:01:41 +0500

    feat: реализовать функционал регистации

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../AuthForm/AuthForm.jsx';
+import * as auth from '../../utils/auth.js';
 import styles from './Register.module.css';
 
 function Register({ title, name, submitButtonText }) {
+  const [formValue, setFormValue] = useState({
+    email: '',
+    password: ''
+  });
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setFormValue({
+      ...formValue,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    auth.register(formValue.password, formValue.email).then(response => {
+      navigate('/sign-in', { replace: true });
+    });
+  };
+
   return (
     <section className={styles['auth-window']}>
-      <AuthForm title={title} name={name} submitButtonText={submitButtonText} />
+      <AuthForm
+        title={title}
+        name={name}
+        submitButtonText={submitButtonText}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
       <p className={styles['auth-window__text']}>
         Уже зарегистрированы?&nbsp;
         <a className={styles['auth-window__link']} href="/sign-in">
