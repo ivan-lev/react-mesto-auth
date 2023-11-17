32e7400c89fb0984608dac42ae33b8487a6f0259
Ivan Lev <navilev@yandex.ru>
2023-11-17 12:49:57 +0500

    feat: добавить функционал логина

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../AuthForm/AuthForm.jsx';
+import * as auth from '../../utils/auth.js';
 import styles from './Login.module.css';
 
-function Login({ title, name, submitButtonText }) {
+function Login({ title, name, submitButtonText, handleLogin }) {
+  const [formValue, setFormValue] = useState({
+    email: '',
+    password: ''
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setFormValue({
+      ...formValue,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!formValue.email || !formValue.password) {
+      return;
+    }
+    auth
+      .authorize(formValue.password, formValue.email)
+      .then(data => {
+        console.log(data);
+        if (data.token) {
+          setFormValue({ email: '', password: '' });
+          handleLogin();
+          navigate('/', { replace: true });
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <section className={styles['login-window']}>
-      <AuthForm title={title} name={name} submitButtonText={submitButtonText} />
+      <AuthForm
+        title={title}
+        name={name}
+        submitButtonText={submitButtonText}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </section>
   );
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,7 +4,6 @@ export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      //Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ password, email })
@@ -13,8 +12,25 @@ export const register = (password, email) => {
       return response.json();
     })
     .then(res => {
-      console.log(res);
       return res;
+    })
+    .catch(err => console.log(err));
+};
+
+export const authorize = (password, email) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ password, email })
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        return data;
+      }
     })
     .catch(err => console.log(err));
 };
