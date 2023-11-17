1ad11208f8c428a75a2445afd4239a24fedc6a5f
Ivan Lev <navilev@yandex.ru>
2023-11-17 12:00:19 +0500

    feat: доработать форму

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './AuthForm.module.css';
 
-function AuthForm({ title, name, submitButtonText, ...props }) {
+function AuthForm({ title, name, submitButtonText, onChange, onSubmit, ...props }) {
   return (
     <>
       <p className={`popup__heading ${styles['auth-form__heading']}`}>{title}</p>
@@ -9,29 +9,31 @@ function AuthForm({ title, name, submitButtonText, ...props }) {
         id="popup__register-form"
         name={`popup_type_${name}`}
         className={`popup__form ${styles['auth-form__form']}`}
-        //onSubmit={onSubmit}
+        onSubmit={onSubmit}
       >
         <fieldset className="popup__fieldset">
           <input
             type="email"
             placeholder="Email"
-            id="register-email"
-            name="register-email"
+            id="email"
+            name="email"
             className={`popup__input ${styles['auth-form__input']}`}
             required
+            onChange={onChange}
           />
-          <span className="popup__error avatar-photo-link-error"></span>
+          <span className="popup__error"></span>
         </fieldset>
         <fieldset className="popup__fieldset">
           <input
             type="password"
             placeholder="Пароль"
-            id="register-password"
-            name="register-password"
+            id="password"
+            name="password"
             className={`popup__input ${styles['auth-form__input']}`}
+            onChange={onChange}
             required
           />
-          <span className="popup__error avatar-photo-link-error"></span>
+          <span className="popup__error"></span>
         </fieldset>
 
         <button type="submit" className={`button ${styles['auth-form__button']}`}>
