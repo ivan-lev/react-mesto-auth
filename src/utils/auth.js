14f714e0a48eda1f779864a0ee99bf05a0e33d27
Ivan Lev <fanatos@mail.ru>
2023-11-19 10:11:35 +0500

    fix: исправить обработку ответа сервера

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,20 +1,23 @@
 export const BASE_URL = 'https://auth.nomoreparties.co';
 
 export const register = (password, email) => {
-  return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ password, email })
-  })
-    .then(response => {
-      return response.json();
+  return (
+    fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ password, email })
     })
-    .then(res => {
-      return res;
-    })
-    .catch(err => console.log(err));
+      .then(response => {
+        // return response.json();
+        return response;
+      })
+      // .then(res => {
+      //   return res;
+      // })
+      .catch(err => console.log(err))
+  );
 };
 
 export const authorize = (password, email) => {
