670c844a14537a22aa9063234da7d5043defc657
Ivan Lev <fanatos@mail.ru>
2023-11-19 10:13:32 +0500

    fix: исправить отображение инфы в InfoTooltip

--- src/components/InfoToolTip/InfoTooltip.jsx
+++ src/components/InfoToolTip/InfoTooltip.jsx
@@ -1,21 +1,18 @@
-//import React, { useState } from 'react';
+import React from 'react';
 import styles from './InfoTooltip.module.css';
 
-const InfoToolTip = ({ icon, message, isOpened, onClose }) => {
-  //const [isOpened, setIsOpened] = useState(false);
-
+const InfoToolTip = ({ content, isOpened }) => {
   return (
     <div className={`popup ${isOpened ? 'popup_opened' : ''}`}>
-      {/* <div className={`popup popup_opened`}> */}
       <div className={`popup__container ${styles['infotooltip-container']}`}>
-        <img className={styles['infotooltip-icon']} src={icon} alt="Иконка" />
-        <div className={styles['infotooltip-message']}>{message}</div>
+        <img className={styles['infotooltip-icon']} src={content.icon} alt="Иконка" />
+        <div className={styles['infotooltip-message']}>{content.message}</div>
 
         <button
           id="close-photo-window-button"
           type="button"
           className="button popup__close-button"
-          onClick={onClose}
+          onClick={content.onClose}
         ></button>
       </div>
     </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import AuthForm from '../AuthForm/AuthForm.jsx';
+import InfoToolTip from '../InfoToolTip/InfoTooltip.jsx';
 import * as auth from '../../utils/auth.js';
 import styles from './Register.module.css';
+
+import successIcon from '../../images/icon-sucsess.svg';
+import errorIcon from '../../images/icon-error.svg';
 
 function Register({ title, name, submitButtonText }) {
   const [formValue, setFormValue] = useState({
     email: '',
     password: ''
   });
+  const [registrationStatus, setRegistrationStatus] = useState(null);
+  const [showTooltipWindow, setShowTooltipWindow] = useState(false);
+
+  const tooltipTypes = {
+    success: {
+      icon: successIcon,
+      message: 'Вы успешно зарегистрировались!',
+      onClose: () => {
+        setShowTooltipWindow(false);
+        navigate('/sign-in', { replace: true });
+        setRegistrationStatus(null);
+      }
+    },
+    error: {
+      icon: errorIcon,
+      message: 'Что-то пошло не так! Попробуйте ещё раз.',
+      onClose: () => {
+        setShowTooltipWindow(false);
+        setRegistrationStatus(null);
+      }
+    }
+  };
+
   const navigate = useNavigate();
 
   const handleChange = e => {
     const { name, value } = e.target;
-
     setFormValue({
       ...formValue,
       [name]: value
@@ -22,9 +48,19 @@ function Register({ title, name, submitButtonText }) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    auth.register(formValue.password, formValue.email).then(response => {
-      navigate('/sign-in', { replace: true });
-    });
+    auth
+      .register(formValue.password, formValue.email)
+      .then(response => {
+        if (response.status === 400) {
+          setRegistrationStatus(false);
+        } else {
+          setRegistrationStatus(true);
+        }
+        setShowTooltipWindow(true);
+      })
+      .catch(error => {
+        console.log('Ошибка регистрации' + error);
+      });
   };
 
   return (
@@ -38,10 +74,17 @@ function Register({ title, name, submitButtonText }) {
       />
       <p className={styles['auth-window__text']}>
         Уже зарегистрированы?&nbsp;
-        <a className={styles['auth-window__link']} href="/sign-in">
+        <Link className={styles['auth-window__link']} to="/sign-in">
           Войти
-        </a>
+        </Link>
       </p>
+
+      {showTooltipWindow && (
+        <InfoToolTip
+          content={registrationStatus ? tooltipTypes.success : tooltipTypes.error}
+          isOpened={showTooltipWindow}
+        />
+      )}
     </section>
   );
 }
