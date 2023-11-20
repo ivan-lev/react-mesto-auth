6195fad3f55372e3b262c782f560586421298722
Ivan Lev <fanatos@mail.ru>
2023-11-20 14:26:46 +0500

    feat: реализовать подписку тултипа на контент

--- src/components/InfoToolTip/InfoTooltip.jsx
+++ src/components/InfoToolTip/InfoTooltip.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+
+import { TooltipContext } from '../../contexts/tooltipContext.js';
+
 import styles from './InfoTooltip.module.css';
 
-const InfoToolTip = ({ content, isOpened }) => {
+const InfoToolTip = ({ isOpened, onClose, toShow }) => {
+  const content = React.useContext(TooltipContext);
+
   return (
     <div className={`popup ${isOpened ? 'popup_opened' : ''}`}>
       <div className={`popup__container ${styles['infotooltip-container']}`}>
-        <img className={styles['infotooltip-icon']} src={content.icon} alt="Иконка" />
-        <div className={styles['infotooltip-message']}>{content.message}</div>
+        <img className={styles['infotooltip-icon']} src={content[toShow].icon} alt="Иконка" />
+        <div className={styles['infotooltip-message']}>{content[toShow].message}</div>
 
         <button
           id="close-photo-window-button"
           type="button"
           className="button popup__close-button"
-          onClick={content.onClose}
+          onClick={onClose}
         ></button>
       </div>
     </div>
